29d3fbbe57a89a881d74d65d01b4689240e1cf01
Roma <105862211+roma-perceptron@users.noreply.github.com>
2022-06-03 14:29:26 +0300

    Update genie.py

--- genie.py
+++ genie.py
@@ -475,7 +475,7 @@ class GAN_Enjoy_Nice_Interface_Easy():
     lower_box.add_class('lower_box')
 
     # upload stylesheet file
-    with open('/utils/styles.css', mode='r') as f:
+    with open('utils/styles.css', mode='r') as f:
       data_input_style = f.read()
 
     # apply styles
