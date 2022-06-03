2bfe91d62fda0c6d61dc76f245943c6986174e70
Roma <105862211+roma-perceptron@users.noreply.github.com>
2022-06-03 14:55:21 +0300

    Update genie_texts.py

--- genie_texts.py
+++ genie_texts.py
@@ -3,6 +3,10 @@
 class GENIE_Texts():
     def __init__(self, lang='rus'):
         self.lang = lang
+        
+    def hello(self):
+        text = {'rus': rus_hello, 'eng': eng_hello}
+        print(text[self.lang])
 
     def example(self):
         text = {'rus': rus_example, 'eng': eng_example}
